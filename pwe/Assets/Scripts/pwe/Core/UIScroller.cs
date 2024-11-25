2d1a30c904f633d3ef9103a7b107de5d569c1500
Agustin Pontura <pontura@gmail.com>
2024-11-25 16:36:18 -0300

    nuevos rives y oven

--- pwe/Assets/Scripts/pwe/Core/UIScroller.cs
+++ pwe/Assets/Scripts/pwe/Core/UIScroller.cs
@@ -8,6 +8,7 @@ namespace Pwe.Core
     public class UIScroller : MonoBehaviour
     {
         [SerializeField] Transform container;
+        [SerializeField] float speed =  0.03f;
         float separation_y;
         int id = 0;
         int total;
@@ -21,7 +22,7 @@ namespace Pwe.Core
         void Update()
         {
             float dest_y = id * separation_y * -1;
-            float _y = Mathf.Lerp(container.transform.localPosition.y, dest_y, 0.05f);
+            float _y = Mathf.Lerp(container.transform.localPosition.y, dest_y, speed);
             container.transform.localPosition = new Vector2(container.transform.localPosition.x, _y);
         }
         public void Scroll(bool top)
