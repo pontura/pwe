0d140c668f6a04a76b718fb4921cad19627dccb1
Agustin Pontura <pontura@gmail.com>
2024-11-28 21:15:00 -0300

    outro

--- pwe/Assets/Scripts/pwe/games/cooking/Cutscene.cs
+++ pwe/Assets/Scripts/pwe/games/cooking/Cutscene.cs
@@ -6,8 +6,10 @@ namespace Pwe.Games.Cooking
 {
     public class Cutscene : GameMain
     {
+        [SerializeField] RiveRawImage riveRawImage;
         [SerializeField] ButtonUI skipButton;
         [SerializeField] bool backToContinue;
+        [SerializeField] string riveName = "Cooking/cutscenes/outro.riv";
 
         public override void OnInitialize()
         {
@@ -15,8 +17,11 @@ namespace Pwe.Games.Cooking
         }
         public override void OnInit()
         {
+            if(riveRawImage != null) // TO-DO
+                riveRawImage.Init(riveName, OnLoaded);
             base.OnInit();
         }
+        void OnLoaded() { }
         void Skip()
         {
             if (backToContinue)
